5ba49fe392612525cb28de57621c04eb64141dfc
Anders <dzhiwei@cn.ibm.com>
2016-09-30 18:03:30 +0800

    add css module

--- DswLinks/app/app.component.ts
+++ DswLinks/app/app.component.ts
@@ -30,84 +30,19 @@ const LINKOBJS: LinkObject[] = [
 
 @Component({
     selector: 'my-app',
-    template: `
-    <table class="data-table-1" border="0" cellpadding="0" cellspacing="0">
-      <caption><em>DSW Links</em></caption>
-      <tr>
-        <th>Application</th>
-        <th>Production</th>
-        <th>Support</th>
-        <th>PS-DST</th>
-        <th>UAT</th>
-        <th>FVT</th>
-        <th>Pre-prod</th>
-        <th>Devpr</th>
-        <th>Local</th>
-
-      </tr>
-      <tr *ngFor='let linkObj of linkObjArr'>
-        <td>{{linkObj.nickName}}</td>
-        <td><a href="{{linkObj.prodUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.psUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.psDstUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.uatUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.fvtUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.preProdUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.devprUrl}}">{{linkObj.nickName}}</a></td>
-        <td><a href="{{linkObj.localUrl}}">Customer</a></td>
-      </tr>
-      </table>
-
-  `,
+    templateUrl: 'app/link-list.component.html',
     styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .heroes {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .heroes li {
-      cursor: pointer;
+    .bs-example {
       position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
+      padding: 5px 5px 5px;
+      margin: 0 -15px 15px;
+      border-color: #e5e5e5 #eee #eee;
+      border-style: solid;
+      border-width: 1px 0;
+      -webkit-box-shadow: inset 0 3px 6px rgba(0,0,0,.05);
+      box-shadow: inset 0 3px 6px rgba(0,0,0,.05);
     }
-    .heroes li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .heroes li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .heroes .text {
-      position: relative;
-      top: -3px;
-    }
-    .heroes .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `]
+    `]
 })
 export class AppComponent {
     title = 'DSW links';
