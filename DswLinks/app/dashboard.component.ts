5b99a21a1fbf53fd5061fb71670bf227d09ebb3c
Anders <dzhiwei@cn.ibm.com>
2016-10-10 18:09:47 +0800

    add dashboard template

--- DswLinks/app/dashboard.component.ts
+++ DswLinks/app/dashboard.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { LinkObject } from './link-object';
+import { LinkObjectService } from './link-object.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-dashboard',
-  template: `
-    <h3>My Dashboard.</h3>
-  `
+  templateUrl: 'dashboard.compoment.html'
 })
-export class DashboardComponent {  }
+export class DashboardComponent implements OnInit{
+  links : LinkObject[] = [];
+
+  constructor(private linkObjectService: LinkObjectService){  }
+
+  ngOnInit(): void {
+    this.linkObjectService.getLinkObjs().then(links => this.links = links.slice(1,3));
+  }
+  gotoDetail(link: LinkObject): void {
+
+  }
+}
